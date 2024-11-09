e5c212224f665bbe9080cc887722b4fa6f865328
Cecil Foubert <Cfoubert0782@conestogac.on.ca>
2024-11-09 12:06:43 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,8 @@ int main ()
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
 	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
+
+	InitWindow(1600, 1000, "LairHMI");
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
